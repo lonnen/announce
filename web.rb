5ba7962ff43004f97b8398f87fd200a4bce0c4c0
lonnen <chris.lonnen@gmail.com>
2013-05-05 16:26:48 -0700

    basic auth and a test endpoint

--- web.rb
+++ web.rb
@@ -1,6 +1,14 @@
 require 'sinatra'
 
+use Rack::Auth::Basic, "Restricted" do |username, password|
+  username == 'admin' and password == 'admin'
+end
+
 get '/ping' do
   "Pong"
 end
 
+post '/' do
+  push = JSON.parse(params[:payload])
+  "I got some JSON: #{push.inspect}"
+end
